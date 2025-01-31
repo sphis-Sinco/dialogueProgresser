db889e0ff339e29a486997e7355f072e6bdac936
IdealistCat <sinconsistencia@gmail.com>
2025-01-30 20:49:50 -0600

    split dialogue Setup into a function

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -17,21 +17,7 @@ class PlayState extends FlxState
 
 	override public function create()
 	{
-		dialogueFile = FileManager.getJSON(FileManager.getDataFile('dummy.json'));
-
-		dialogueBox.makeGraphic(Std.int(FlxG.width / 1.05), Std.int(FlxG.height / 2));
-		dialogueBox.screenCenter();
-		dialogueBox.y = FlxG.height - dialogueBox.height - 16;
-		add(dialogueBox);
-
-		dialogue_currentPage = dialogueFile.first_page - 1;
-		dialogueText.text = dialogueFile.pages[dialogue_currentPage].dialogue;
-		dialogueText.size = 16;
-		dialogueText.color = 0x000000;
-		dialogueText.setPosition(dialogueBox.x, dialogueBox.y);
-		dialogueText.fieldWidth = dialogueBox.width;
-		dialogueText.fieldHeight = dialogueBox.height;
-		add(dialogueText);
+		dialogueSetup();
 
 		super.create();
 	}
@@ -51,5 +37,23 @@ class PlayState extends FlxState
 			dialogueText.text = dialogueFile.pages[dialogue_currentPage].dialogue;
 
 		super.update(elapsed);
+	}
+	public function dialogueSetup()
+	{
+		dialogueFile = FileManager.getJSON(FileManager.getDataFile('dummy.json'));
+
+		dialogueBox.makeGraphic(Std.int(FlxG.width / 1.05), Std.int(FlxG.height / 2));
+		dialogueBox.screenCenter();
+		dialogueBox.y = FlxG.height - dialogueBox.height - 16;
+		add(dialogueBox);
+
+		dialogue_currentPage = dialogueFile.first_page - 1;
+		dialogueText.text = dialogueFile.pages[dialogue_currentPage].dialogue;
+		dialogueText.size = 16;
+		dialogueText.color = 0x000000;
+		dialogueText.setPosition(dialogueBox.x, dialogueBox.y);
+		dialogueText.fieldWidth = dialogueBox.width;
+		dialogueText.fieldHeight = dialogueBox.height;
+		add(dialogueText);
 	}
 }
