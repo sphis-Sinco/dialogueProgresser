c6ba98e5dbf7a45d27a3478987d5936831f67154
IdealistCat <sinconsistencia@gmail.com>
2025-01-30 20:07:59 -0600

    dialogue box text

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5,12 +5,14 @@ import FileManager;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.text.FlxText;
 
 class PlayState extends FlxState
 {
 	var dialogueFile:DialogueJSON;
 
 	var dialogueBox:FlxSprite = new FlxSprite();
+	var dialogueText:FlxText = new FlxText();
 
 	override public function create()
 	{
@@ -20,6 +22,14 @@ class PlayState extends FlxState
 		dialogueBox.screenCenter();
 		dialogueBox.y = FlxG.height - dialogueBox.height - 16;
 		add(dialogueBox);
+
+		dialogueText.text = dialogueFile.pages[dialogueFile.first_page - 1].dialogue;
+		dialogueText.size = 16;
+		dialogueText.color = 0x000000;
+		dialogueText.setPosition(dialogueBox.x, dialogueBox.y);
+		dialogueText.fieldWidth = dialogueBox.width;
+		dialogueText.fieldHeight = dialogueBox.height;
+		add(dialogueText);
 
 		super.create();
 	}
