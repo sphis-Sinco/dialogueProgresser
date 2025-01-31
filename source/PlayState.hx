0b777d44bb2ec09144969bf23864f8317aa58ccc
IdealistCat <sinconsistencia@gmail.com>
2025-01-30 19:04:38 -0600

    reading dummy dialogue file and FMV 7

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,11 +1,17 @@
 package;
 
+import DialogueJSON;
+import FileManager;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	var dialogueFile:DialogueJSON;
+
 	override public function create()
 	{
+		dialogueFile = FileManager.getJSON(FileManager.getDataFile('dummy.json'));
+
 		super.create();
 	}
 
